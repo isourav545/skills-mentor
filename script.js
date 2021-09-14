10f4b15ddc33531d64109aca9d180d9580a3b401
Sourav Ghosh <39117406+isourav545@users.noreply.github.com>
2021-09-14 09:57:46 +0530

    add important code files

---
+++ script.js
@@ -0,0 +1,12 @@
+gsap.from('.links',{duration:3,x:100, delay:1,opactity:0,ease:"power2.out"})
+
+gsap.from('#logo',{duration:3,x:-100,delay:1,opactity:0,ease:"power2.out"})
+
+
+gsap.from('h3',{opacity:0,duration:1,delay:1,x:'-100'});
+
+gsap.from('h2',{opacity:0,duration:1,delay:2,x:'100'});
+
+gsap.from('button',{opacity:0,duration:1,delay:2,x:'-100'});
+
+gsap.from('footer',{opacity:0,duration:1,delay:1,y:'100'});
